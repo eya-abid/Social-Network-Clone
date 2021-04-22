7cf7446d030ef4a80fbbf4a6c239cc27060764df
eya <eyaabid90@gmail.com>
2021-04-22 22:49:43 +0100

    ng routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {
+  path: 'signin',
+  component: SigninComponent
+  },
+  
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+      },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
